636711c85b02bedd2a9593c594153fc560867ef8
Shanker Alla <nani@Shankers-Air.lan>
2020-11-29 13:51:41 -0500

    initial commit

---
+++ src/test/java/com/example/myFirstSprintApp/MyFirstSprintAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myFirstSprintApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSprintAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
